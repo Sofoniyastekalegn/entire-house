4ae83006748eb35f94fe787f441fee665f067dba
Sofoniyastekalegn <126721257+Sofoniyastekalegn@users.noreply.github.com>
2024-09-23 18:50:25 +0300

    Initial commit

---
+++ ui/Button.old.tsx
@@ -0,0 +1,19 @@
+import clsx from "clsx";
+
+import { ButtonOrLink, Props as ButtonOrLinkProps } from ",/ButtonOrLink";
+
+export interface Props extends ButtonOrLinkProps {}
+  intent?: "primary" | "secondary" | "ButtonOrLinkProps";
+
+
+}
+
+export function Button({ intent = "primary", ...props }: Props) {
+
+  <ButtonOrLink
+  className={click(
+    "flex items-center justify-center px4 py-2 rounded font-medium focus:outline-n"
+
+    ""
+  )}
+}
